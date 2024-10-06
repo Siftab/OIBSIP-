03b2254f688728dfa726fd27cef87598d6f0bfde
Siftab <siiffuuu@gmail.com>
2024-10-06 20:31:17 +0600

    feat:main route created and implemented to app

--- pizza-ordering-backend/src/app.ts
+++ pizza-ordering-backend/src/app.ts
@@ -1,11 +1,16 @@
-import express from 'express'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-const app = express()
-const port = 3000
+import express from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import router from "./app/routes";
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.use(globalErrorHandler)
-
-export default app
+app.use("/api/pizza/", router);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use(globalErrorHandler);
+// handle invalid Api
+app.use("*", (req, res) => {
+  res.send({ success: false, message: "Api not Found", data: "" });
+});
+export default app;
--- pizza-ordering-backend/src/app/routes/index.ts
+++ pizza-ordering-backend/src/app/routes/index.ts
@@ -1,2 +1,16 @@
 import {Router} from 'express'
+import { userRoute } from '../modules/Users/user.route'
 const router = Router()
+
+
+
+const moduleRoutes =[{
+    path:'/user',
+    route:userRoute
+}]
+
+moduleRoutes.forEach((route) => router.use(route.path,route.route));
+
+
+
+export default router
